ab7833876a950f96d22a64143b5d9b4bd79da80c
Yunus <yunusozdemir468@gmail.com>
2021-06-05 22:58:09 +0300

    employee.prototype.ShowInfos()

--- OOP/ProtoType/app.js
+++ OOP/ProtoType/app.js
@@ -2,12 +2,25 @@
 // object.name = "Mustafa";
 // console.log(object);
 
+// function Employee(name, age) {
+//     this.name = name;
+//     this.age = age;
+//     this.showInfos = function() {
+//         console.log("Bilgiler Gösteriliyor");
+//     }
+// }
+// const emp1 = new Employee("Yunus", 18);
+// console.log(emp1);
+
+
 function Employee(name, age) {
     this.name = name;
     this.age = age;
-    this.showInfos = function() {
-        console.log("Bilgiler Gösteriliyor");
-    }
+}
+Employee.prototype.showInfos = function() {
+    console.log("İsim: " + this.name + " Yaş: " + this.age);
 }
 const emp1 = new Employee("Yunus", 18);
+const emp2 = new Employee("Mustafa", 25);
+emp2.showInfos();
 console.log(emp1);
