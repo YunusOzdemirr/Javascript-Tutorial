98b5388830ce5854fae5c0bbc7eb1622219c6b42
Yunus <yunusozdemir468@gmail.com>
2021-06-05 15:44:29 +0300

    Spread Operator

---
+++ SpreadOperator/app.js
@@ -0,0 +1,21 @@
+//Spread Operator
+
+// const a = langs = ["Python", "C++", "C#", "Php"];
+// console.log(langs[0], langs[1], langs[2], langs[3]);
+// console.log(...langs)
+
+// const langs2 = ["Javascript", "C#", langs[0], langs[1], langs[2], langs[3]];
+// const langs2 = ["Javascript", "C#", ...langs];
+// console.log(langs2);
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const [a, b, ...number2] = numbers;
+// console.log(a, b);
+// console.log(number2);
+
+const addNumbers = (a, b, c) => console.log(a + b + c);
+
+const numbers = [100, 200, 300];
+
+addNumbers(...numbers);
