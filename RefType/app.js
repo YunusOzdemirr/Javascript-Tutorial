dd3fa395d6c2ac547b59e7c38ecd99b7133d03a3
Yunus <yunusozdemir468@gmail.com>
2021-06-05 21:41:10 +0300

    Referans Type

---
+++ RefType/app.js
@@ -0,0 +1,34 @@
+// let a = "Yunus";
+// let b = "Yunus";
+
+// if (a === b) {
+//     console.log("Eşit")
+// }
+
+// let array1 = [1, 2, 3, 4, 5];
+// let array2 = [1, 2, 3, 4, 5];
+
+
+// if (array1 === array2) {
+//     console.log(Eşit);
+// }
+
+// const person1 = {
+//     name: "Mustafa",
+//     age: 25
+// };
+// const person2 = {
+//     name: "Mustafa",
+//     age: 25
+// };
+
+// if (person1 === person2) {
+//     console.log(Eşit);
+// }
+
+const cities = new Map();
+cities.set("Ankara", 5);
+cities.set("İstanbul", 15);
+const key = [1, 2, 3]
+cities.set(key, "Array");
+console.log(cities.get(key))
